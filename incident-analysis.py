3b23fec86d5725dff6c2a59ab46e56932bb5b9ce
diddes-se <github@diddes.se>
2025-10-09 12:17:09 +0200

    added week numbers, added most costly incidents and some formating  and comments

--- incident-analysis.py
+++ incident-analysis.py
@@ -13,12 +13,13 @@ def safe_float(value, default=0):
         return float(value) if value else default
     except ValueError:
         return default
+        
 
 with open("network_incidents.csv", encoding="utf8") as f:
     network_incidents_raw = list(csv.DictReader(f))
 
 
-
+# format the input from the .csv file
 network_incidents = [{
     "ticket_id": incident["ticket_id"],
     "week_number": safe_int(incident["week_number"]),
@@ -36,10 +37,15 @@ network_incidents = [{
     
 } for incident in network_incidents_raw]
 
+
 # Create variables to store values
-unique_weeks = set()
 total_incidents_by_severity = {"critical": 0, "high": 0, "medium": 0, "low": 0}
 incidents_with_high_users = ""
+incidents_and_cost = ""
+incidents_with_highest_cost_formated = ""
+
+# add all week numbers to a set
+unique_weeks = sorted({incident["week_number"] for incident in network_incidents})
 
 # Read out data on incidents
 for incident in network_incidents:
@@ -61,14 +67,23 @@ for incident in network_incidents:
         total_incidents_by_severity["low"] += +1
 
     # List all incidents that impacted more than 100 users
-    if incident["affected_users"] < 100:
+    if incident["affected_users"] > 100:
         incidents_with_high_users += ("  " + incident["ticket_id"] + "  " + incident["severity"].ljust(16)  + incident["description"] + "\n")
 
+# Get the five most costly incidents
+incidents_with_highest_cost = sorted(network_incidents, key=lambda x: x["cost_sek"], reverse=True)[:5]
 
+# sort out and format highest cost to report
+for row in incidents_with_highest_cost:
+    incidents_with_highest_cost_formated += ( "  " + row["ticket_id"].ljust(17)
+                                            + row["site"].ljust(20)
+                                            + str(row["cost_sek"]).rjust(10) + " kr  "
+                                            + row["description"]  + "\n"
+                                            )
 
 
 with open('incident_analysis.txt', 'w', encoding='utf-8') as f:
-    f.write("Incidentanalys TechCorp AB för vecka:" + "\n")
+    f.write("Incidentanalys TechCorp AB för vecka: " + str(unique_weeks).strip("[]") + "\n")
     f.write("="*50 + "\n\n")
     f.write("Antal incidenter per alvarighetsgrad:\n")
     f.write("-"*30 + "\n")
@@ -80,3 +95,7 @@ with open('incident_analysis.txt', 'w', encoding='utf-8') as f:
     f.write("-"*30 + "\n")
     f.write("  Ticket id".ljust(17) + "Alvarighetsgrad".ljust(16) + "Beskrivning\n")
     f.write(incidents_with_high_users + "\n\n")
+    f.write("De fem dyraste incidenterna:\n")
+    f.write("-"*30 + "\n")
+    f.write("  Ticket id".ljust(19) + "Site".ljust(20) + "Kostnad".ljust(15) + "Beskrivning\n")
+    f.write(incidents_with_highest_cost_formated + "\n")
